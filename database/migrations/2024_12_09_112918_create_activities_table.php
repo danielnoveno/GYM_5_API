c73d32b3575a2153eb0c918e2726f3562b7115fa
danielnoveno <220711663@students.uajy.ac.id>
2024-12-09 19:51:54 +0700

    update activity

--- database/migrations/2024_12_09_112918_create_activities_table.php
+++ database/migrations/2024_12_09_112918_create_activities_table.php
@@ -10,7 +10,7 @@ return new class extends Migration
     {
         Schema::create('activities', function (Blueprint $table) {
             $table->id();
-            $table->string('event');
+            $table->string('activity');
             $table->timestamp('finish_at')->nullable();
             $table->date('date');
             $table->timestamps();
