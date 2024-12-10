6699edef5e9f0e01d4439b15e872b145517cf235
danielnoveno <220711663@students.uajy.ac.id>
2024-12-11 06:00:42 +0700

    hapus table tidak digunakan

--- routes/api.php
+++ routes/api.php
@@ -30,12 +30,9 @@ Route::post('/login', [PelangganController::class, 'login']);
 Route::post('/logout', [PelangganController::class, 'logout'])->middleware('auth:sanctum');
 
 Route::resource('alat_gym', AlatGymController::class);
-Route::resource('coach', CoachController::class);
-Route::resource('detail_transaksi', DetailTransaksiController::class);
 Route::resource('jadwal', JadwalController::class);
 Route::resource('jenis_membership', JenisMembershipController::class);
 Route::resource('kelas_olahraga', KelasOlahragaController::class);
-Route::resource('layanan', LayananController::class);
 Route::resource('membership', MembershipController::class);
 Route::resource('pelanggan', PelangganController::class);
 Route::resource('personal_trainer', PersonalTrainerController::class);
